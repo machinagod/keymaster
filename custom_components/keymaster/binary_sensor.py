13b41826bbb70e13325e014619fae88a927aac6f
firstof9 <firstof9@gmail.com>
2021-12-10 15:06:33 -0700

    add ZHA to network sensor

--- custom_components/keymaster/binary_sensor.py
+++ custom_components/keymaster/binary_sensor.py
@@ -65,6 +65,11 @@ try:
 except (ModuleNotFoundError, ImportError):
     pass
 
+try:
+    from homeassistant.components.zha.core.const import DOMAIN as ZHA_DOMAIN
+except (ModuleNotFoundError, ImportError):
+    pass
+
 _LOGGER = logging.getLogger(__name__)
 ENTITY_NAME = "Network"
 
@@ -129,6 +134,53 @@ class BaseNetworkReadySensor(BinarySensorEntity):
         self._attr_is_on = value_to_set
         if write_state:
             self.async_write_ha_state()
+
+
+class ZHANetworkReadySensor(BaseNetworkReadySensor):
+    """Binary sensor to indicate whether or not `zha` device is available."""
+
+    def __init__(
+        self, primary_lock: KeymasterLock, child_locks: List[KeymasterLock]
+    ) -> None:
+        """Initialize sensor."""
+        super().__init__(primary_lock, child_locks, ZHA_DOMAIN)
+        self.lock_config_entry_id = None
+        self._lock_found = True
+        self.ent_reg = None
+        self._attr_should_poll = True
+
+    async def async_update(self) -> None:
+        """Update sensor."""
+        if not self.ent_reg:
+            self.ent_reg = async_get_entity_registry(self.hass)
+
+        if (
+            not self.lock_config_entry_id
+            or not self.hass.config_entries.async_get_entry(self.lock_config_entry_id)
+        ):
+            entity_id = self.primary_lock.lock_entity_id
+            lock_ent_reg_entry = self.ent_reg.async_get(entity_id)
+
+            if not lock_ent_reg_entry:
+                if self._lock_found:
+                    self._lock_found = False
+                    _LOGGER.warning("Can't find your lock %s.", entity_id)
+                return
+
+            self.lock_config_entry_id = lock_ent_reg_entry.config_entry_id
+
+            if not self._lock_found:
+                _LOGGER.info("Found your lock %s", entity_id)
+                self._lock_found = True
+
+        network_ready = self.primary_lock.lock_entity_id.available
+
+        # If network_ready and self._attr_is_on are both true or both false, we don't need
+        # to do anything since there is nothing to update.
+        if not network_ready ^ self.is_on:
+            return
+
+        self.async_set_is_on_property(network_ready, False)
 
 
 class ZwaveJSNetworkReadySensor(BaseNetworkReadySensor):
